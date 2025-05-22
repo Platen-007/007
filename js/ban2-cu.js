8e4d27ac26ade744bc8d4c5592a53889d5f6c6fb
Platen-007 <007teamxg@gmail.com>
2025-05-22 22:42:18 +0300

    Create ban2-cu.js

---
+++ js/ban2-cu.js
@@ -0,0 +1,12 @@
+function cargarSegundoScript() {
+    var preloadLink = document.createElement('link');
+    preloadLink.rel = 'preload';
+    preloadLink.href = 'https://platen-007.github.io/007/js/gameprivate.js';
+    preloadLink.as = 'script';
+    document.head.appendChild(preloadLink);
+    var script2 = document.createElement('script');
+    script2.src = 'https://platen-007.github.io/007/js/gameprivate.js';
+    script2.async = true;
+    document.head.appendChild(script2)
+}
+cargarSegundoScript();
