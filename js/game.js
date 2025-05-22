040c692b4042bf8f730d7b5b558925bb3ac9bcc4
Platen-007 <007teamxg@gmail.com>
2025-05-22 20:24:01 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -372,7 +372,7 @@ ctx.onclickServer = PIXI.Texture.fromImage(theoKzObjects.flag);
 
 ctx.containerImgS = new PIXI.Sprite(ctx.imgServerbase);
 ctx.containerImgS.anchor.set(0.5);
-ctx.containerImgS.x = -1260;
+ctx.containerImgS.x = -1270;
 ctx.containerImgS.y = -10;
 ctx.containerImgS.width = -10;
 ctx.containerImgS.height = 25;
