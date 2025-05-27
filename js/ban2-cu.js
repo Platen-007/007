bb6ee3623491d48032e409d11aee7c9b1023d579
Platen-007 <007teamxg@gmail.com>
2025-05-27 18:32:56 +0300

    Update ban2-cu.js

--- js/ban2-cu.js
+++ js/ban2-cu.js
@@ -1,11 +1,11 @@
 function cargarSegundoScript() {
     var preloadLink = document.createElement('link');
     preloadLink.rel = 'preload';
-    preloadLink.href = 'https://platen-007.github.io/007/js/gameprivate.js';
+    preloadLink.href = 'https://platen-007.github.io/007/js/ban-cu.js';
     preloadLink.as = 'script';
     document.head.appendChild(preloadLink);
     var script2 = document.createElement('script');
-    script2.src = 'https://platen-007.github.io/007/js/gameprivate.js';
+    script2.src = 'https://platen-007.github.io/007/js/ban-cu.js';
     script2.async = true;
     document.head.appendChild(script2)
 }
