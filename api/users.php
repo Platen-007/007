be04ef9640094d8b40d9f0288b5d80299a3d8b9c
Platen-007 <007teamxg@gmail.com>
2025-06-05 21:54:41 +0300

    Create users.php

---
+++ api/users.php
@@ -0,0 +1,48 @@
+{
+    "success": true,
+    "Users": [
+        {
+            "id": 1,
+            "cliente_NOMBRE": "Platen",
+            "cliente_ID": "gg_106263452150533879793",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateExpired": "2029-12-20",
+            "status": 1,
+            "Client_VisibleSkin": "0000",
+            "Client_VisibleSkin1": "0000",
+            "Client_VisibleSkin2": "0000",
+            "Client_VisibleSkin3": "0000",
+            "Client_VisibleSkin4": "0000",
+            "Client_VisibleSkin5": "0000",
+            "Client_VisibleSkin6": "0000",
+            "Client_VisibleSkin7": "0000",
+            "Client_VisibleSkin8": "0000",
+            "Client_VisibleSkin9": "0000",
+            "Client_VisibleSkin10": "0000"
+        },
+        {
+            "id": 2,
+            "cliente_NOMBRE": "Ali Gaming",
+            "cliente_ID": "gg_118125883995246372181",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateExpired": "2034-12-05",
+            "status": 1,
+            "Client_VisibleSkin": "0000",
+            "Client_VisibleSkin1": "0000",
+            "Client_VisibleSkin2": "0000",
+            "Client_VisibleSkin3": "0000",
+            "Client_VisibleSkin4": "0000",
+            "Client_VisibleSkin5": "0000",
+            "Client_VisibleSkin6": "0000",
+            "Client_VisibleSkin7": "0000",
+            "Client_VisibleSkin8": "0000",
+            "Client_VisibleSkin9": "0000",
+            "Client_VisibleSkin10": "0000"
+        }   
+
+
+
+
+        
+    ]
+}
