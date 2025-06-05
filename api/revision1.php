70348249baf3f8aac775da4c61d95310bc319496
Platen-007 <007teamxg@gmail.com>
2025-06-05 22:45:26 +0300

    Update revision1.php

--- api/revision1.php
+++ api/revision1.php
@@ -57,7 +57,26 @@
             "Client_VisibleSkin8": "0000",
             "Client_VisibleSkin9": "0000",
             "Client_VisibleSkin10": "0000"
-}        
+        },
+        {
+            "id": 4,
+            "cliente_NOMBRE": "FİRE YT",
+            "cliente_ID": "gg_109051383568783034005",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateExpired": "2025-09-05",
+            "status": 1,
+            "Client_VisibleSkin": "0000",
+            "Client_VisibleSkin1": "0000",
+            "Client_VisibleSkin2": "0000",
+            "Client_VisibleSkin3": "0000",
+            "Client_VisibleSkin4": "0000",
+            "Client_VisibleSkin5": "0000",
+            "Client_VisibleSkin6": "0000",
+            "Client_VisibleSkin7": "0000",
+            "Client_VisibleSkin8": "0000",
+            "Client_VisibleSkin9": "0000",
+            "Client_VisibleSkin10": "0000"
+        }    
 
 
 
